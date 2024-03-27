26cc21c2b23ddaed453b8d1b64708498d84e119d
Tiziano26 <132932851+Tiziano26@users.noreply.github.com>
2024-03-26 21:28:46 -0300

    Clase persona

---
+++ pruebaUwU/src/main/java/com/mycompany/pruebauwu/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebauwu;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    
+    public String saludar(){
+        return "Hola soy "+ nombre +" "+ apellido +" y tengo "+ edad +"";
+    } 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
+
+
+
